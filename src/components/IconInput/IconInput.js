a3ceaa1392e72a6bf11aaa23d44881b2d3ae55dc
Sebastian Pieczyński <testament777@gmail.com>
2021-10-01 15:40:24 +0200

    Base for Input and ProgressBar

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,41 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const Wrapper = styled.div`
+	display: flex;
+`;
+
+const IconWrapper = styled(Icon)`
+	border-bottom: 2px solid ${COLORS.black};
+	color: ${COLORS.gray700};
+	padding-right: 0.2rem;
+	& > svg {
+		height: 80%;
+		padding-top: 0.2rem;
+		margin-bottom: -0.2rem;
+		stroke-width: 2px;
+	}
+`;
+
+const SearchInput = styled.input`
+	border: none;
+	border-bottom: 2px solid ${COLORS.black};
+	outline: none;
+`;
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+	return (
+		<Wrapper>
+			<IconWrapper id={icon}></IconWrapper>
+			<SearchInput
+				size={size}
+				width={width}
+				type="search"
+				name={label}
+				placeholder={placeholder}
+			></SearchInput>
+		</Wrapper>
+	);
 };
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,28 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+	return (
+		<Wrapper>
+			<Progress value={value} max={100}></Progress>
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div``;
+
+const Progress = styled.progress`
+	background-color: #f3f3f3;
+	border: 0;
+	height: 16px;
+	border-radius: 4px;
+	padding: 3px;
+	width: 200px;
+	box-shadow: 0px 1px 4px 1px inset ${COLORS.transparentGray15};
+	&::-moz-progress-bar {
+		/* style rules */
+		border-radius: 4px 0 0 4px;
+		background-color: ${COLORS.primary};
+	}
+`;
 
 export default ProgressBar;
